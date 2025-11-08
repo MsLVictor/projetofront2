ef8673eed9d322a3370b671507947ec4df6cdbad
MsLVictor <victormleite25@icloud.com>
2025-11-08 12:04:56 -0300

    incluindo arquivos de inicio de projeto

---
+++ js/contato.js
@@ -0,0 +1,10 @@
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const msg = document.getElementById('msg');
+const botaoEnviar = document.getElementById('button')
+
+
+function enviarMensagem(){
+
+}
+
